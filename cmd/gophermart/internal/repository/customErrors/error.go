7f41e5bc780476095b72474c005b03252e8047dd
Bond <test@no-example.ru>
2025-07-10 16:25:57 +0300

    done

--- cmd/gophermart/internal/repository/customErrors/error.go
+++ cmd/gophermart/internal/repository/customErrors/error.go
@@ -2,7 +2,7 @@ package customerrors
 
 import "errors"
 
-var ErrOrderUploadedByAnotherUser = errors.New("Order Uploaded By Another User")
-var ErrOrderAlreadyUploadedBySameUser = errors.New("Order Uploaded By Same User")
-var ErrInsufficientBalance = errors.New("Insufficient Balance")
-var ErrInvalidOrderNumber = errors.New("Invalid order number")
+var ErrOrderUploadedByAnotherUser = errors.New("order uploaded by another user")
+var ErrOrderAlreadyUploadedBySameUser = errors.New("order uploaded by same user")
+var ErrInsufficientBalance = errors.New("insufficient balance")
+var ErrInvalidOrderNumber = errors.New("invalid order number")
